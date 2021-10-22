e9e9fa74d2a70fcc8d49561a115dfb17cd2b7386
riittagerhards <rgerhards@outlook.com>
2021-10-22 14:48:30 +0200

    💄 Add final styling

--- components/quoteCard.js
+++ components/quoteCard.js
@@ -1,10 +1,10 @@
 import { createElement } from "../lib/elements";
 import styles from "./quoteCard.module.css";
 
-export default function createQuoteCard() {
+export default function createQuoteCard({ q, a }) {
   const quoteCard = createElement("article", { className: styles.card }, [
-    createElement("p", { className: styles.quote, textContent: "q" }),
-    createElement("h4", { className: styles.author, textContent: "a" }),
+    createElement("p", { className: styles.quote, textContent: `"${q}"` }),
+    createElement("h4", { className: styles.author, textContent: `- ${a}` }),
   ]);
 
   return quoteCard;
--- main.js
+++ main.js
@@ -3,17 +3,25 @@ import { createElement } from "./lib/elements";
 import { createHeaderElement } from "./components/headerComponent.js";
 import createQuoteCard from "./components/quoteCard.js";
 
-function renderApp() {
+async function renderApp() {
   const appElement = document.querySelector("#app");
 
   const headerElement = createHeaderElement();
+
+  const response = await fetch(
+    "https://cors.machens.koeln/https://zenquotes.io/api/quotes"
+  );
+
+  const quotes = await response.json();
+
+  const quoteCards = quotes.map((quote) => createQuoteCard(quote));
 
   const mainElement = createElement(
     "main",
     {
       className: "main",
     },
-    [createQuoteCard()]
+    quoteCards
   );
 
   appElement.append(headerElement, mainElement);
