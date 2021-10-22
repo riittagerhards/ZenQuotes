a452f1fb63d4604424de2ff99e4bd509278ac4be
riittagerhards <rgerhards@outlook.com>
2021-10-22 12:33:57 +0200

    ✨ Add quote card component

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 import "./style.css";
+import { createElement } from "./lib/elements";
 import { createHeaderElement } from "./components/headerComponent.js";
+import createQuoteCard from "./components/quoteCard.js";
 
 function renderApp() {
   const appElement = document.querySelector("#app");
 
   const headerElement = createHeaderElement();
-  appElement.append(headerElement);
+
+  const mainElement = createElement(
+    "main",
+    {
+      className: "main",
+    },
+    [createQuoteCard()]
+  );
+
+  appElement.append(headerElement, mainElement);
 }
 
 renderApp();
