c3e3ad183c38caac98370477f9f3a67ddbab9387
riittagerhards <rgerhards@outlook.com>
2021-10-25 11:46:07 +0200

    💄 Fix responsive styling for header

--- components/headerComponent.js
+++ components/headerComponent.js
@@ -1,7 +1,7 @@
 import { createElement } from "../lib/elements";
 
 export function createHeaderElement() {
-  const headerElement = createElement("header", {}, [
+  const headerElement = createElement("header", { className: "header" }, [
     createElement("img", {
       className: "titlePic",
       src: "https://freesvg.org/img/Emerald-Lotus-Flower-No-Background.png",
